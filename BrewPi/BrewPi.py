cd14843202164a1e736a98f6e87ece4e5a4e7288
FinnHeu <Finn.Heukamp@t-online.de>
2022-01-16 21:00:45 +0100

    added __main__

--- BrewPi/BrewPi.py
+++ BrewPi/BrewPi.py
@@ -1,7 +1,7 @@
 # Main function for BrewPi
 
-from BrewPi.initialize_devices import Initialize
-from BrewPi.user_inputs import UserInputMaischenRasten
+from .initialize_devices import Initialize
+from .user_inputs import UserInputMaischenRasten
 
 def BrewPi():
     """
@@ -13,16 +13,5 @@ def BrewPi():
     rast_min, rast_temp, ein_temp, ab_temp = UserInputMaischenRasten()
 
     return
-
-
-if __name__ == '__main__':
-
-    try:
-        BrewPi()
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-
-
-
 
 
---
+++ BrewPi/__main__.py
@@ -0,0 +1,11 @@
+# Main function for BrewPi
+
+from .BrewPi import BrewPi
+
+
+BrewPi()
+
+
+
+
+
