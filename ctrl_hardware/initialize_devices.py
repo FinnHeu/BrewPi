7426c4aacc69accdd04591aadeaa70391e2f8f21
FinnHeu <Finn.Heukamp@t-online.de>
2022-02-01 16:47:28 +0100

    restructured

--- ctrl_hardware/initialize_devices.py
+++ ctrl_hardware/initialize_devices.py
@@ -125,8 +125,8 @@ def InitTests(lcd, device_file, ledPin_Socket_A):
         temp_diff = MeanTemp(device_file, consistency_check=True)
         # check deviation from mean temperature
         if any(t > .5 for t in temp_diff):
-            LCD(lcd, str1='Temp:' + str(round(temp_diff[0])) + 'C ',
-                str2=str(round(temp_diff[1],3)) + 'C ' + str(round(temp_diff[2])) + 'C ')
+            LCD(lcd, str1='Temp:' + str(round(temp_diff[0], 3)) + 'C ',
+                str2=str(round(temp_diff[1], 3)) + 'C ' + str(round(temp_diff[2]), 3) + 'C ')
             time.sleep(3)
             LCD(lcd, str1='Thermistors are ',
                  str2='calibrated...')
